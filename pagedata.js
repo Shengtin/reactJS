13e823f557e86f951d8ed21750186751195261d4
Shengtin <shengt521@gmail.com>
2022-03-21 14:56:00 +0800

    test

---
+++ pagedata.js
@@ -0,0 +1,62 @@
+var page = [
+	{
+		title: "Introduction",
+		pages: 5
+	},
+	{
+		title: "Javascript ES6",
+		pages: 5
+	},
+	{
+		title: "React Playground - Getting Started",
+		pages: 4
+	},
+	{
+		title: "React Playground - React Basics",
+		pages: 4
+	},
+	{
+		title: "React Playground - Lets Play",
+		pages: 8
+	},
+	{
+		title: "React Nextagram - Introduction",
+		pages: 2
+	},
+	{
+		title: "React Nextagram - Single Component",
+		pages: 3
+	},
+	{
+		title: "React Nextagram - Multiple Components",
+		pages: 5
+	},
+	{
+		title: "React Nextagram - Frontend Routing",
+		pages: 3
+	},
+	{
+		title: "Handling Forms with React",
+		pages: 2
+	},
+	{
+		title: "React Nextagram - User Authentication",
+		pages: 8
+	},
+	{
+		title: "React Nextagram - Image Uploads",
+		pages: 4
+	},
+	{
+		title: "React Nextagram - Finish Up",
+		pages: 1
+	},
+	{
+		title: "NEXT Chatroom - UI",
+		pages: 3
+	},
+	{
+		title: "NEXT Chatroom - Socket",
+		pages: 3
+	}
+];
